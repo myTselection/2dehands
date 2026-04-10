dfeeccde6d18300c15358b0d8264467ab850b415
myT <myTselection@gmail.com>
2026-04-10 17:10:45 +0200

    Create __init__.py for 2dehands integration

---
+++ custom_components/2dehands/__init__.py
@@ -0,0 +1,6 @@
+"""The 2dehands integration."""
+from homeassistant import config_entries, core
+
+async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
+    """Set up the 2dehands integration."""
+    return True
